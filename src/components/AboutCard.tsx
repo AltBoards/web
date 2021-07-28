8582bf7fb7c802b369f3780d38086673cbf26624
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-07-27 20:58:07 -0400

    Initial TypeScript, I made almost no effort

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 
-export class AboutCard extends Component {
+export interface AboutCardProps {
+  image: string,
+  name: string,
+  subtitle: string,
+  description: string
+}
+
+export interface AboutCardState {
+
+}
+
+export class AboutCard extends Component<AboutCardProps, AboutCardState> {
   static displayName = AboutCard.name;
 
-  render () {
+  render() {
     return (
       <div className="col-sm-3">
         <Card style={{ width: '18rem' }} className="mr-4 mb-4">
